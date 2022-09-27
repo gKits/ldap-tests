29ee0bfb77ef52b838234fca819ffcd58eb4880b
Georgios <gkitsikoudis@gmx.de>
2022-09-27 10:39:16 +0200

    G

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from ldap3 import (
     NTLM
 )
 
-server = Server('172.16.221.2', get_info=ALL, use_ssl=True)
+server = Server('172.16.221.2', get_info=ALL)
 # conn = Connection(server)
 # conn.bind()
 
 conn = Connection(server, auto_bind=True, user='TESTDAP\\Administrator', password='Test123!', authentication=NTLM)
+conn.start_tls()
 conn.extend.standard.who_am_i()
 print(conn)
