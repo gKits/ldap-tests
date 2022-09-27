734fa332e1158d893821019b24b9af260d4b3650
Georgios <gkitsikoudis@gmx.de>
2022-09-27 10:29:18 +0200

    B

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ server = Server('172.16.221.2', get_info=ALL)
 # conn = Connection(server)
 # conn.bind()
 
-conn = Connection(server, auto_bind=True, user='DAP\\Administrator', password='Test123!', authentication=NTLM)
+conn = Connection(server, auto_bind=True, user='DAP/Administrator', password='Test123!', authentication=NTLM)
 print(conn)
 conn.extend.who_am_i()
