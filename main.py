c1912467d95bb6e42fdce6d78a83a1d5057005f3
Georgios <gkitsikoudis@gmx.de>
2022-10-10 15:15:48 +0200

    Random user test

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
-from ldap3 import (
-    Server,
-    Connection,
-    ALL,
-    NTLM,
-    MODIFY_ADD,
-    Tls,
-)
-import ssl
+from ldap3 import Server, Connection, ALL, NTLM
+from elizabeth import Text, Address, Personal
+from ldap3.extend.microsoft.addMembersToGroups import ad_add_members_to_groups as addUsersToGroups
+import random
 
-tls_config = Tls(
-    validate=ssl.CERT_NONE,
-    # version=ssl.PROTOCOL_TLSv1
-)
-server = Server('172.16.221.2', get_info=ALL, use_ssl=False, tls=tls_config)
-# conn = Connection(server)
-# conn.bind()
+serverName = 'AD-DAP_TEST.testdap.com'
+connUser = 'TESTDAP\\Administrator'
+connUserPwd = 'Test123!'
+usersOU = 'OU=test-ou,DC=testdap,DC=com'
+groupsDN = 'CN=test-groups,OU=test-ou,DC=testdap,DC=com'
 
-conn = Connection(server, auto_bind=True, user='TESTDAP\\Administrator', password='Test123!', authentication=NTLM)
+groupsDnList = []
+usersDnList = []
 
-conn.search(search_base='cn=Administrator,DC=ad-dap_test,DC=testdap,DC=com', search_filter='(sAMAccountName=Administrator)')
-print(conn.result)
 
-group_dn = 'CN=TestGroup2,OU=TestOU,DC=testdap,DC=com'
-user_dn = 'CN=Test User1,OU=TestOU,DC=testdap,DC=com'
-conn.modify(group_dn, [MODIFY_ADD, 'member', [user_dn]])
-print(conn.result)
+server = Server(serverName, get_info=ALL)
+conn = Connection(server, user=connUser, password=connUserPwd, authentication=NTLM)
+conn.bind()
+
+conn.add(usersOU, 'organizationalUnit')
+conn.add(groupsDN, 'organizationalUnit')
+
+data = Text('en')
+for _ in range(0, 10):
+    currentGroup = 'cn=' + data.word() + ',ou=test-groups,dc=stand,dc=local'
+    groupsDnList.append(currentGroup)
+    conn.add(currentGroup, 'group')
+
+address = Address('en')
+person = Personal('en')
+for _ in range(0, 10):
+    address_country = address.country()
+    conn.add('ou=' + address_country + ',ou=test-ou,dc=stand,dc=local', 'organizationalUnit')
+    for _ in range(0, 10):
+        name = person.name(gender='male')
+        surname = person.surname(gender='male')
+        currentUser = 'cn=' + name + '.' + surname + ',' + 'ou=' + address_country + ',ou=test-ou,dc=stand,dc=local'
+        usersDnList.append(currentUser)
+        conn.add(
+            currentUser,
+            'User',
+            {
+                'givenName': name,
+                'sn': surname,
+                'departmentNumber': 'DEV',
+                'telephoneNumber': 1111
+            }
+        )
+
+for _ in range(0, 300):
+    rndUser = random.choice(usersDnList)
+    rndGroup = random.choice(groupsDnList)
+    addUsersToGroups(conn, rndUser, rndGroup)
